f2ae0c368e302d62e84f9cf7ca19d1210d05b3f1
Siddhant <31.siddhant.sharma@gmail.com>
2021-07-30 17:40:37 +0530

    ADD __main__.py and hyena.py Subclassed hyena

---
+++ src/hyena.py
@@ -0,0 +1,408 @@
+import discord
+import ast
+import os
+import asyncpg
+import datetime
+import sqlite3
+import inputimeout
+import random
+import sys
+import asyncio
+import topgg
+import traceback
+
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+
+load_dotenv()
+hyena_intents = discord.Intents.all()
+hyena_intents.presences = False
+
+
+class Hyena(commands.AutoShardedBot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            command_prefix=self._get_hyena_prefix,
+            owner_ids=[711444754080071714, 699543638765731892],
+            intents=hyena_intents,
+            allowed_mentions=discord.AllowedMentions(
+                everyone=False, roles=False, users=True
+            ),
+            description="The main purpose of this bot is to help you manage your discord server in a pretty good way, hyena offers you with a lot of stuff such as AutoModeration, General Moderations, and a lot more.",
+            *args,
+            **kwargs,
+        )
+
+        self.help_command = None
+        self.secrets = {
+            x: y for x, y in os.environ.items() if x in ["TOKEN", "POSTGRES", "TOPGG"]
+        }
+        self.applying = []
+        self.prefix_caches = {}
+        self.get_version = self.get_hyena_version
+        self.get_commands = self.get_total_commands
+        self.hyena_cogs = [
+            f"cogs.{cog[:-3]}"
+            for cog in os.listdir("cogs")
+            if cog.endswith(".py") and not cog.startswith("_")
+        ]
+        self.colours = [0xFFED1B, 0xF1EFE3, 0x00A8FE, 0x1FEDF9, 0x7CF91F, 0xF91F43]
+        from utilities.data import automod as am
+
+        self.automod_handler = am.Detections
+        self.launch_time = datetime.datetime.utcnow()
+        self.topgg = topgg.DBLClient(self, self.secrets["TOPGG"])
+
+    def run(self):
+        try:
+            for cog in self.hyena_cogs:
+                try:
+                    self.load_extension(cog)
+                except Exception as e:
+                    raise e
+        except Exception as e:
+            raise e
+
+        prompt = """
+Which mode do you wanna boot to?
+
+Available Modes: 
+Secure [Only dev Commands]
+Normal [Regular usage]
+CDev [All commands but only devs can run commands]
+
+After 5 seconds hyena will default to normal boot     
+"""
+        try:
+            self.current_mode_type = inputimeout.inputimeout(
+                prompt=prompt + "\n", timeout=5
+            ).lower()
+        except:
+            self.current_mode_type = "normal"
+        print(f"----Continuing with {self.current_mode_type} mode----")
+
+        super().run(self.secrets["TOKEN"])
+
+    async def _get_hyena_prefix(self, hyena, message):
+        base = [f"<@!{hyena.user.id}> ", f"<@{hyena.user.id}>"]
+        prefixes = None
+        try:
+            caches = self.prefix_caches[message.guild.id]
+            prefixes = [*caches, *base]
+        except KeyError:
+            pass
+
+        if prefixes is None:
+            result = await hyena.main_db.fetch(
+                f"SELECT PREFIX FROM guild WHERE GuildID = $1", message.guild.id
+            )
+
+            if result:
+                lst = result[0]["prefix"]
+                lst = ast.literal_eval(lst)
+                prefixes = [*lst, *base]
+                hyena.prefix_caches[message.guild.id] = lst
+            if not result:
+                return commands.when_mentioned_or("-")(hyena, message)
+
+        return prefixes
+
+    async def connect_database(self):
+        self.main_db = await asyncpg.create_pool(
+            database="hyena",
+            user="postgres",
+            password=self.secrets["POSTGRES"],
+            host="127.0.0.1",
+        )
+        self.main_db2 = await asyncpg.create_pool(
+            database="hyena2",
+            user="postgres",
+            password=self.secrets["POSTGRES"],
+            host="127.0.0.1",
+        )
+        self.automod_db = await asyncpg.create_pool(
+            database="hyena_automod",
+            user="postgres",
+            password=self.secrets["POSTGRES"],
+            host="127.0.0.1",
+        )
+
+    def get_hyena_version(self):
+        with open("./assets/version.txt", "r") as f:
+            return f.read()
+
+    def get_total_commands(self, hyena):
+        total = 0
+        for command in hyena.commands:
+            if type(command).__name__ == "Group":
+                total += len(command.commands) + 1
+                continue
+            total += 1
+
+        return total
+
+    clear = lambda self: os.system("cls" if os.name == "nt" else "clear")
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        channel = self.get_channel(794467787988008973)
+        embed = discord.Embed(
+            title="Hyena has been booted up!",
+            colour=random.choice(self.colours),
+            description=f"Logged in as {self.user}.",
+        )
+        try:
+            await channel.send(embed=embed)
+        except:
+            pass
+        self.change_status.start()
+
+        message = "Booting up hyena..."
+        for i in range(2):
+            for i in range(len(message), len(message) - 4, -1):
+                self.clear()
+                print(message[:i])
+                await asyncio.sleep(1)
+        self.clear()
+        print(len(message) * "\b")
+        sys.stdout.write("\033[F")
+        print(f"Logged in as {self.user}.")
+
+    def get_blacklisted_users(self):
+        db = sqlite3.connect("./data/dev.sqlite")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM blacklists")
+        r = cursor.fetchall()
+
+        try:
+            users = r[0][0]
+        except:
+            return []
+
+        users = users.strip("][").split(", ")
+        if users == [""]:
+            return users
+        users = [int(i) for i in users]
+        return users
+
+    async def process_commands(self, message):
+        if message.guild is None or message.author.bot:
+            return
+
+        await super().process_commands(message)
+
+    async def on_message(self, message):
+        if self.current_mode_type == "cdev":
+            if message.author.id not in self.owner_ids:
+                return
+        if message.author.id in self.get_blacklisted_users():
+            return
+
+        await self.process_commands(message)
+
+    def get_cog_aliases(self, term: str):
+        # aliases = {
+        #     ("afk",) : "afk",
+        #     ("application", "applications", "apply") : "application",
+        #     ("automod", "automoderation", "am") : "automod",
+        #     ("command-logs", "cmd_logs", "cmd-logs") : "command-logs",
+        #     ("dev", "developer", "development") : "dev",
+        #     ("fun-cmds", "fun", "fun_commands", "fun_commands") : "fun-cmds",
+        #     ("giveaway", "giveaways", "gws", "gw") : "giveaways",
+        #     ("goodbye-welcome", "leave-join", "welcome", "goodbye") : "goodbye-welcome",
+        #     ("help", "helpcmd") : "help",
+        #     ("hyena-mail", "mail", "hyena_mail") : "hyena-mail",
+        #     ("invites", "invite", "inv") : "invites",
+        #     ("logging", "logs", "log") : "logging",
+        #     ("mod_actions", "actions", "moderation_actions") : "mod_actions",
+        #     ("moderation", "mod") : "moderation",
+        #     ("mute-system", "mute", "mutes") : "mute-system",
+        #     ("prefix", "prefixes", "pre") : "prefix",
+        #     ("roles", "role", "ar", "role-sys") : "roles",
+        #     ("starboard", "sb") : "starboard",
+        #     ("suggestions", "suggestion", "suggest") : "suggestions",
+        #     ("support-server", "hyena-development", "hyena-dev", "ss") : "support-server",
+        #     ("tickets", "ticket") : "tickets",
+        #     ("todo", "todo_list") : "todo",
+        #     ("toggle", "toggles", "enable", "disable") : "toggle",
+        #     ("utils", "utilities", "util") : "utils",
+        #     ("warns", "warn") : "warns",
+        #     ("core-handlers", "handlers", "core") : "core-handlers"
+        # }
+        aliases = {("core-handlers", "handlers", "core"): "core-handlers"}
+
+        for alias, cog in aliases.items():
+            if term.lower() in alias:
+                return cog
+
+    async def handle_load(self, ctx, cog: str):
+        if cog in ["*", "all"]:
+            errored_out = []
+            for cog in self.hyena_cogs:
+                try:
+                    self.load_extension(cog)
+                except commands.errors.ExtensionAlreadyLoaded:
+                    errored_out.append((cog[5:], "This extension is already loaded"))
+                except commands.errors.ExtensionNotFound:
+                    errored_out.append((cog[5:], "This extension was not found"))
+                except Exception as e:
+                    errored_out.append((cog[5:], str(e)))
+            if errored_out == []:
+                return await ctx.send("Successfully completed the operation.")
+
+            newline = "\n"
+
+            await ctx.send(
+                f"""
+Successfully completed the operation. 
+```
+Errored out : {newline.join([f"{x[0]} : {x[1]}" for x in errored_out])}
+```
+"""
+            )
+            return
+
+        if cog.endswith(".py"):
+            cog = cog[:-3]
+
+        cog = self.get_cog_aliases(cog)
+
+        try:
+            self.load_extension(f"cogs.{cog}")
+            await ctx.message.add_reaction("<:OP_Verified:815589801586851840>")
+        except commands.errors.ExtensionAlreadyLoaded:
+            await ctx.send(f"The cog `{cog}` is already loaded")
+        except commands.errors.ExtensionNotFound:
+            await ctx.send(f"The cog `{cog}` is Not found...")
+        except:
+            embed = discord.Embed(
+                title=f"Error while loading ext: {cog}", color=discord.Colour.red()
+            )
+            embed.description = f"""
+    ```py
+    {traceback.format_exc()}
+    ```
+    """
+            await ctx.send(embed=embed)
+
+    async def handle_unload(self, ctx, cog: str):
+        if cog in ["*", "all"]:
+            errored_out = []
+            for cog in self.hyena_cogs:
+                try:
+                    self.unload_extension(cog)
+                except commands.errors.ExtensionNotLoaded:
+                    errored_out.append((cog[5:], "This extension was not loaded"))
+                except commands.errors.ExtensionNotFound:
+                    errored_out.append((cog[5:], "This extension was not found"))
+                except Exception as e:
+                    errored_out.append((cog[5:], str(e)))
+            if errored_out == []:
+                return await ctx.send("Successfully completed the operation.")
+
+            newline = "\n"
+
+            await ctx.send(
+                f"""
+Successfully completed the operation.
+```
+Errored out : {newline.join([f"{x[0]} : {x[1]}" for x in errored_out])}
+```
+"""
+            )
+            return
+
+        if cog.endswith(".py"):
+            cog = cog[:-3]
+
+        cog = self.get_cog_aliases(cog)
+
+        try:
+            self.unload_extension(f"cogs.{cog}")
+            await ctx.message.add_reaction("<:OP_Verified:815589801586851840>")
+        except commands.errors.ExtensionNotLoaded:
+            await ctx.send(f"The cog `{cog}` isn't even loaded.")
+        except commands.errors.ExtensionNotFound:
+            await ctx.send(f"The cog `{cog}` is Not found...")
+        except:
+            embed = discord.Embed(
+                title=f"Error while loading ext: {cog}", color=discord.Colour.red()
+            )
+            embed.description = f"""
+    ```py
+    {traceback.format_exc()}
+    ```
+    """
+            await ctx.send(embed=embed)
+
+    async def handle_reload(self, ctx, cog: str):
+        if cog in ["*", "all"]:
+            errored_out = []
+            for cog in self.hyena_cogs:
+                try:
+                    self.reload_extension(cog)
+                except commands.errors.ExtensionNotLoaded:
+                    errored_out.append((cog[5:], "This extension was not loaded"))
+                except commands.errors.ExtensionNotFound:
+                    errored_out.append((cog[5:], "This extension was not found"))
+                except Exception as e:
+                    errored_out.append((cog[5:], str(e)))
+            if errored_out == []:
+                return await ctx.send("Successfully completed the operation.")
+
+            newline = "\n"
+            return await ctx.send(
+                f"""
+    Successfully completed the operation. 
+    ```
+    Errored out : {newline.join([f"{x[0]} : {x[1]}" for x in errored_out])}
+    ```
+    """
+            )
+
+        if cog.endswith(".py"):
+            cog = cog[:-3]
+
+        cog = self.get_cog_aliases(cog)
+
+        try:
+            self.reload_extension(f"cogs.{cog}")
+            await ctx.message.add_reaction("<:OP_Verified:815589801586851840>")
+        except commands.errors.ExtensionNotLoaded:
+            await ctx.send(f"The cog `{cog}` isn't even loaded.")
+        except commands.errors.ExtensionNotFound:
+            await ctx.send(f"The cog `{cog}` is Not found...")
+        except:
+            embed = discord.Embed(
+                title=f"Error while loading ext: {cog}", color=discord.Colour.red()
+            )
+            embed.description = f"""
+    ```py
+    {traceback.format_exc()}
+    ```
+    """
+            await ctx.send(embed=embed)
+
+    @tasks.loop(seconds=20)
+    async def change_status(self):
+        status = [
+            discord.Streaming(name="On -help", url="https://bit.ly/hyena-bot"),
+            discord.Game(name="With the BAN HAMMER! | -help"),
+            discord.Activity(
+                type=discord.ActivityType.watching, name="The Chat! | -help"
+            ),
+            discord.Game(
+                name=f"On {len(self.guilds)} Guilds With {len(self.users)} Users | -help"
+            ),
+            discord.Activity(
+                type=discord.ActivityType.listening,
+                name="the mods While eating Donuts | -help",
+            ),
+            discord.Activity(
+                type=discord.ActivityType.competing, name="With Other Bots! | -help"
+            ),
+            discord.Game(name="Got a new upgrade im v3.0 | -help"),
+        ]
+        await self.change_presence(
+            activity=random.choice(status), status=discord.Status.dnd
+        )
---
+++ src/utilities/data/automod.py
@@ -0,0 +1,261 @@
+import discord
+import datetime
+import aiohttp
+import re
+import json
+
+
+def default_filters():
+    with open("./data/filtered-words.json") as f:
+        return json.load(f)
+
+
+class GuildConfig:
+    def __init__(self, hyena, guild: discord.Guild):
+        self.hyena = hyena
+        self.guild = guild
+        self.db = self.hyena.automod_db
+        self.default_filters = default_filters()
+
+    async def fetch_toggles(self):
+        r = await self.db.fetchrow(
+            "SELECT * FROM toggles WHERE guild_id = $1", self.guild.id
+        )
+        if r:
+            return dict(r)
+        return None
+
+    async def get_filtered_words(self):
+        res = await self.db.fetch(
+            "SELECT blacklisted FROM config WHERE guild_id = $1", self.guild.id
+        )
+        if (not res) or (res[0]["blacklisted"] in [[], None]):
+            return self.default_filters
+        return res[0]["blacklisted"]
+
+    async def get_ignored_channels(self):
+        res = await self.db.fetch(
+            "SELECT ignored_channels FROM config WHERE guild_id = $1", self.guild.id
+        )
+        if (not res) or (res[0]["ignored_channels"] in [[], None]):
+            return None
+        return res[0]["ignored_channels"]
+
+    async def get_spam_limit(self):
+        res = await self.db.fetch(
+            "SELECT spam_limit FROM config WHERE guild_id = $1", self.guild.id
+        )
+        if not res or res[0]["spam_limit"] == None:
+            return 4
+        return res[0]["spam_limit"]
+
+    async def get_mention_limit(self):
+        res = await self.db.fetch(
+            "SELECT mention_limit FROM config WHERE guild_id = $1", self.guild.id
+        )
+        if not res or res[0]["mention_limit"] == None:
+            return 5
+        return res[0]["mention_limit"]
+
+    async def get_caps_limit(self):
+        res = await self.db.fetch(
+            "SELECT caps_limit FROM config WHERE guild_id = $1", self.guild.id
+        )
+        if not res or res[0]["caps_limit"] == None:
+            return 70
+        return res[0]["caps_limit"]
+
+
+class Detections:
+    def __init__(self, hyena):
+        self.hyena = hyena
+        self.URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+        self.INVITE_REGEX = re.compile(
+            r"((http(s|):\/\/|)(discord)(\.(gg|io|me)\/|app\.com\/invite\/)([0-z]+))"
+        )
+        self.nsfw_cache = {}  # soon™
+
+    # --------------------------- HELPER -------------------------------------
+
+    async def is_ignored(self, guild, channel):
+        ignored = await GuildConfig(self.hyena, guild).get_ignored_channels()
+        if ignored:
+            return channel.id in ignored
+
+    def find_urls(self, string):
+        url = re.findall(self.URL_REGEX, string)
+        return [x[0] for x in url]
+
+    async def api_nsfw_detector(self, url):
+        async with aiohttp.ClientSession() as cs:
+            headers = {"Api-Key": "eb09ecb1-b9d0-43bc-af3d-8df58e4907d3"}
+            data = {"image": url}
+            async with cs.post(
+                "https://api.deepai.org/api/nsfw-detector", headers=headers, data=data
+            ) as r:
+                return await r.json()
+
+    async def is_enabled(self, type: str, guild):
+        toggles = await GuildConfig(self.hyena, guild).fetch_toggles()
+        if not toggles:
+            return False
+        return toggles[type]
+
+    async def format_string(self, string: str):
+        string_encode = string.encode("ascii", "ignore")
+        string = string_encode.decode()
+        string = string.lower()
+        lst = string.split(" ")
+        lst = [x.strip() for x in lst if x != ""]
+        lst = ["".join([char for char in x if char.isalpha()]) for x in lst]
+
+        return tuple(lst)
+
+    # --------------------------- HANDLERS -------------------------------------
+
+    async def handle_spam(self, message: discord.Message):
+        if not await self.is_enabled("spam", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+        messages = list(
+            filter(
+                lambda m: m.author == message.author
+                and (datetime.datetime.utcnow() - m.created_at).seconds < 10,
+                self.hyena.cached_messages,
+            )
+        )
+
+        limit = await GuildConfig(self.hyena, message.guild).get_spam_limit()
+        if len(messages) > limit:
+            return True
+
+    async def handle_nsfw(self, message: discord.Message):
+        if not await self.is_enabled("nsfw", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or message.channel.is_nsfw()
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+
+        urls = self.find_urls(message.content)
+        attachments = [x.url for x in message.attachments]
+        image_urls = sorted(set([*urls, *attachments]))
+
+        if len(urls) == 0:
+            return False
+
+        for url in image_urls:
+            res = await self.api_nsfw_detector(url)
+            if res.get("err") is not None:
+                continue
+            try:
+                score = res["output"]["nsfw_score"]
+                if score >= 0.8:
+                    return True
+            except KeyError:
+                pass
+
+    async def handle_invites(self, message: discord.Message):
+        if not await self.is_enabled("invites", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+        invite_match = self.INVITE_REGEX.findall(message.content)
+        if invite_match:
+            for i in invite_match:
+                try:
+                    invite = await self.hyena.fetch_invite(i[-1])
+                except discord.NotFound:
+                    pass
+                else:
+                    if not invite.guild.id == message.guild.id:
+                        return True
+
+    async def handle_mentions(self, message: discord.Message):
+        if not await self.is_enabled("mentions", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+        mentions = []
+        for i in message.mentions:
+            if i not in mentions and i != message.author and not i.bot:
+                mentions.append(i)
+
+        limit = await GuildConfig(self.hyena, message.guild).get_mention_limit()
+        if len(mentions) >= limit:
+            return True
+
+    async def handle_urls(self, message: discord.Message):
+        if not await self.is_enabled("urls", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+        urls = self.find_urls(message.content)
+        if urls:
+            return True
+
+    async def handle_filters(self, message: discord.Message):
+        if not await self.is_enabled("filtered_words", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+        filters = await GuildConfig(self.hyena, message.guild).get_filtered_words()
+        if filters is None:
+            return False
+        words = await self.format_string(message.content)
+
+        for word in words:
+            if word in filters:
+                return True
+
+    async def handle_caps(self, message: discord.Message):
+        if not await self.is_enabled("capitals", message.guild):
+            return False
+        if (
+            not message.guild
+            or message.author.bot
+            or await self.is_ignored(message.guild, message.channel)
+        ):
+            return False
+        limit = await GuildConfig(self.hyena, message.guild).get_caps_limit()
+        count = 0
+        length = len(message.content)
+
+        if length < 5:
+            return False
+
+        for char in message.content:
+            if char.isupper():
+                count += 1
+
+        try:
+            percent = count / length * 100
+        except:
+            return False
+
+        if percent >= limit:
+            return True
