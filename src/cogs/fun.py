2c5ac7bdcd28a187805e3109766c9374c8eeb243
Siddhant <31.siddhant.sharma@gmail.com>
2021-08-09 22:27:59 +0530

    Fix [Formatted correctly]

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -1,26 +1,24 @@
-import discord
-import aiohttp
 import io
 import random
+
+import aiohttp
+import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
-
-
 
 
 class ImageFun(commands.Cog):
     def __init__(self, hyena, colors):
         self.hyena = hyena
         self.hyena.colors = colors
-        
-    
+
     @property
     def data(self):
         return ["Image Fun"]
 
-# commands:
- 
- # Triggered
+    # commands:
+
+    # Triggered
 
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
@@ -29,19 +27,23 @@ class ImageFun(commands.Cog):
             member = ctx.author
 
         async with aiohttp.ClientSession() as trigSession:
-            async with trigSession.get(f'https://some-random-api.ml/canvas/triggered?avatar={member.avatar.url}') as trigImg: # get users avatar as png with 1024 size
-                triggerData = io.BytesIO(await trigImg.read()) 
-                file = discord.File(triggerData, 'triggered.gif')
+            async with trigSession.get(
+                f"https://some-random-api.ml/canvas/triggered?avatar={member.avatar.url}"
+            ) as trigImg:  # get users avatar as png with 1024 size
+                triggerData = io.BytesIO(await trigImg.read())
+                file = discord.File(triggerData, "triggered.gif")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} is triggered", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://triggered.gif')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} is triggered", icon_url=member.avatar.url
+                )
+                embed.set_image(url="attachment://triggered.gif")
 
                 await trigSession.close()
 
+                await ctx.send(file=file, embed=embed)
 
-                await ctx.send(file = file, embed = embed) 
-# wasted 
+    # wasted
 
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
@@ -50,18 +52,23 @@ class ImageFun(commands.Cog):
             member = ctx.author
 
         async with aiohttp.ClientSession() as wastedSession:
-            async with wastedSession.get(f'https://some-random-api.ml/canvas/wasted?avatar={member.avatar.url}') as wasteImg: # get users avatar as png with 1024 size
-                wastedData = io.BytesIO(await wasteImg.read()) 
-                file = discord.File(wastedData, 'wasted.png')
+            async with wastedSession.get(
+                f"https://some-random-api.ml/canvas/wasted?avatar={member.avatar.url}"
+            ) as wasteImg:  # get users avatar as png with 1024 size
+                wastedData = io.BytesIO(await wasteImg.read())
+                file = discord.File(wastedData, "wasted.png")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} has been wasted :|", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://wasted.png')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} has been wasted :|", icon_url=member.avatar.url
+                )
+                embed.set_image(url="attachment://wasted.png")
 
                 await wastedSession.close()
-                
-                await ctx.send(file = file, embed = embed)
-# mission passed
+
+                await ctx.send(file=file, embed=embed)
+
+    # mission passed
 
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
@@ -70,19 +77,24 @@ class ImageFun(commands.Cog):
             member = ctx.author
 
         async with aiohttp.ClientSession() as passedSession:
-            async with passedSession.get(f'https://some-random-api.ml/canvas/passed?avatar={member.avatar.url}') as passImg: # get users avatar as png with 1024 size
-                passedData = io.BytesIO(await passImg.read()) 
-                file = discord.File(passedData, 'passed.png')
+            async with passedSession.get(
+                f"https://some-random-api.ml/canvas/passed?avatar={member.avatar.url}"
+            ) as passImg:  # get users avatar as png with 1024 size
+                passedData = io.BytesIO(await passImg.read())
+                file = discord.File(passedData, "passed.png")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} has passed the mission :)", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://passed.png')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} has passed the mission :)",
+                    icon_url=member.avatar.url,
+                )
+                embed.set_image(url="attachment://passed.png")
 
                 await passedSession.close()
- 
-                await ctx.send(file = file, embed = embed)
 
-# jail 
+                await ctx.send(file=file, embed=embed)
+
+    # jail
 
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
@@ -91,19 +103,24 @@ class ImageFun(commands.Cog):
             member = ctx.author
 
         async with aiohttp.ClientSession() as jailSession:
-            async with jailSession.get(f'https://some-random-api.ml/canvas/jail?avatar={member.avatar.url}') as jailedImg: # get users avatar as png with 1024 size
-                jailData = io.BytesIO(await jailedImg.read()) 
-                file = discord.File(jailData, 'jailed.png')
+            async with jailSession.get(
+                f"https://some-random-api.ml/canvas/jail?avatar={member.avatar.url}"
+            ) as jailedImg:  # get users avatar as png with 1024 size
+                jailData = io.BytesIO(await jailedImg.read())
+                file = discord.File(jailData, "jailed.png")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} was just sent to jail ;-;", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://jailed.png')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} was just sent to jail ;-;",
+                    icon_url=member.avatar.url,
+                )
+                embed.set_image(url="attachment://jailed.png")
 
                 await jailSession.close()
-                
-                await ctx.send(file = file, embed = embed)
 
-# comrade
+                await ctx.send(file=file, embed=embed)
+
+    # comrade
 
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
@@ -112,19 +129,24 @@ class ImageFun(commands.Cog):
             member = ctx.author
 
         async with aiohttp.ClientSession() as comradeSession:
-            async with comradeSession.get(f'https://some-random-api.ml/canvas/comrade?avatar={member.avatar.url}') as comradeImg: # get users avatar as png with 1024 size
-                comradeData = io.BytesIO(await comradeImg.read()) 
-                file = discord.File(comradeData, 'comrade.png')
+            async with comradeSession.get(
+                f"https://some-random-api.ml/canvas/comrade?avatar={member.avatar.url}"
+            ) as comradeImg:  # get users avatar as png with 1024 size
+                comradeData = io.BytesIO(await comradeImg.read())
+                file = discord.File(comradeData, "comrade.png")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} was forced to become a comrade ???", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://comrade.png')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} was forced to become a comrade ???",
+                    icon_url=member.avatar.url,
+                )
+                embed.set_image(url="attachment://comrade.png")
 
                 await comradeSession.close()
-                
-                await ctx.send(file = file, embed = embed)
 
-# pixelssssssssss
+                await ctx.send(file=file, embed=embed)
+
+    # pixelssssssssss
 
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
@@ -133,47 +155,59 @@ class ImageFun(commands.Cog):
             member = ctx.author
 
         async with aiohttp.ClientSession() as pixelSession:
-            async with pixelSession.get(f'https://some-random-api.ml/canvas/pixelate?avatar={member.avatar.url}') as pixelImg: # get users avatar as png with 1024 size
-                pixelData = io.BytesIO(await pixelImg.read()) 
-                file = discord.File(pixelData, 'pixelated.png')
+            async with pixelSession.get(
+                f"https://some-random-api.ml/canvas/pixelate?avatar={member.avatar.url}"
+            ) as pixelImg:  # get users avatar as png with 1024 size
+                pixelData = io.BytesIO(await pixelImg.read())
+                file = discord.File(pixelData, "pixelated.png")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} why did u even do this ?", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://pixelated.png')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} why did u even do this ?",
+                    icon_url=member.avatar.url,
+                )
+                embed.set_image(url="attachment://pixelated.png")
 
                 await pixelSession.close()
-                
 
-                await ctx.send(file = file, embed = embed)
-# hmmmm
-    
+                await ctx.send(file=file, embed=embed)
+
+    # hmmmm
+
     @commands.command()
     @commands.cooldown(1, 3, BucketType.user)
-    async def comment(self, ctx, member: discord.Member = None, *,comment = "Next time provide a comment nub."):
+    async def comment(
+        self,
+        ctx,
+        member: discord.Member = None,
+        *,
+        comment="Next time provide a comment nub.",
+    ):
         if not member:
             member = ctx.author
 
         async with aiohttp.ClientSession() as ytSession:
-            async with ytSession.get(f'https://some-random-api.ml/canvas/youtube-comment?avatar={member.avatar.url}&username={member.name}&comment={comment}') as commnt: # get users avatar as png with 1024 size
-                commentData = io.BytesIO(await commnt.read()) 
-                file = discord.File(commentData, 'pixelated.png')
+            async with ytSession.get(
+                f"https://some-random-api.ml/canvas/youtube-comment?avatar={member.avatar.url}&username={member.name}&comment={comment}"
+            ) as commnt:  # get users avatar as png with 1024 size
+                commentData = io.BytesIO(await commnt.read())
+                file = discord.File(commentData, "pixelated.png")
 
-                embed = discord.Embed(color = random.choice(self.hyena.colors))
-                embed.set_author(name = f"{member.name} Just commented", icon_url= member.avatar.url)
-                embed.set_image(url = 'attachment://pixelated.png')
+                embed = discord.Embed(color=random.choice(self.hyena.colors))
+                embed.set_author(
+                    name=f"{member.name} Just commented", icon_url=member.avatar.url
+                )
+                embed.set_image(url="attachment://pixelated.png")
 
                 await ytSession.close()
-                
-                await ctx.send(file = file, embed = embed)
 
+                await ctx.send(file=file, embed=embed)
 
-                 
-        
-        
 
 # ---------------------------- End of Image Fun ---------------------------------
 
 # The text fun commands go below
+
 
 class TextFun(commands.Cog):
     def __init__(self, hyena, colors):
@@ -184,20 +218,18 @@ class TextFun(commands.Cog):
     def data(self):
         return ["Text Fun"]
 
-# commands:
+    # commands:
 
     @commands.command()
     async def sus(self, ctx):
         await ctx.send("SUSU")
 
 
-
-    
-
 # ---------------------------- End of Text Fun ---------------------------------
-  
+
 
 # setup stuff
+
 
 def setup(hyena):
     hyena.add_cog(TextFun(hyena, hyena.colors))
