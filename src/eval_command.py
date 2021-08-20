674a6db71f653755d829aca6ffb0d2b00dbf7439
AHiddenDonut <31.siddhant.sharma@gmail.com>
2021-08-20 03:23:04 +0000

    formatted

--- src/eval_command.py
+++ src/eval_command.py
@@ -1,3 +1,8 @@
-import asyncio, discord
-async def code(ctx, hyena): 
-   for i in range(19): await ctx.send(" <@699543638765731892>  ", delete_after=0)
+import asyncio
+
+import discord
+
+
+async def code(ctx, hyena):
+    for i in range(19):
+        await ctx.send(" <@699543638765731892>  ", delete_after=0)
